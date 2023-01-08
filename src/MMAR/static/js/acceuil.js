0c105484d356906c885cdc52aeba00af0c8efaf9
agnesduke <www.agnesduke@gmail.com>
2023-01-08 07:12:14 +0100

    page supprimercli societe

---
+++ src/MMAR/static/js/acceuil.js
@@ -0,0 +1,34 @@
+new Morris.Line({
+    // ID of the element in which to draw the chart.
+    element: 'myfirstchart',
+    // Chart data records -- each entry in this array corresponds to a point on
+    // the chart.
+    data: [
+      { year: '2023', value: 3 },
+      { year: '2024', value: 00 },
+      { year: '2025', value: 00 },
+      { year: '2026', value: 00 },
+      { year: '2027', value: 00 }
+    ],
+    // The name of the data record attribute that contains x-values.
+    xkey: 'year',
+    // A list of names of data record attributes that contain y-values.
+    ykeys: ['value'],
+    // Labels for the ykeys -- will be displayed when you hover over the
+    // chart.
+    labels: ['Value']
+  });
+
+  Morris.Donut({
+    element: 'donut-example',
+    data: [
+      {label: "services plus sollicite", value: 78},
+      {label: "services moins sollicites", value: 12},
+    
+    ],
+    colors:['blue', 'yellow'],
+    labelColor:'#ffffff'
+  });
+
+  
+  
